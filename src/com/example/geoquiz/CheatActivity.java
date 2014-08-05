c69af53a3acc08174718e6f5a18f9fbc9a37328a
Amrut Patil <axp7911@rit.edu>
2014-08-04 22:18:55 -0400

    GeoQuiz Android App
    Simple quiz app that ask users different geographical questions and
    lets you know whether a given user is cheating.

---
+++ src/com/example/geoquiz/CheatActivity.java
@@ -0,0 +1,54 @@
+package com.example.geoquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CheatActivity extends Activity {
+	
+	public static final String EXTRA_ANSWER_IS_TRUE="com.example.geoquiz.answer_is_true";
+	public static final String EXTRA_ANSWER_SHOWN="com.example.geoquiz.answer_shown";
+	
+	private TextView mAnswerTextView;
+	private Button mShowAnswerButton;
+	private boolean mAnswerIsTrue;
+	
+	private void setAnswerShownResult(boolean isAnswerShown){
+		Intent data = new Intent();
+		data.putExtra(EXTRA_ANSWER_SHOWN,isAnswerShown);
+		setResult(RESULT_OK,data);
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cheat);
+		
+	mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+		
+		mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
+		
+		mShowAnswerButton = (Button)findViewById(R.id.showAnswerButton);
+		
+		setAnswerShownResult(false);
+		
+		mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				if(mAnswerIsTrue){
+					mShowAnswerButton.setText(R.string.true_button);
+				}else{
+					mShowAnswerButton.setText(R.string.false_button);
+				}
+				
+				setAnswerShownResult(true);
+			}
+		});
+		
+	}
+
+}
